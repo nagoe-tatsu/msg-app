8dc9b0e6a1af7fbc93ef69d3f0662e0c0c60d70a
nagoe-tatsu <lifeap.nagoe@gmail.com>
2018-11-20 16:55:40 +0900

    show2アクション追加

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,4 +2,8 @@ class MessagesController < ApplicationController
   def show
     @msg = params[:msg] 
   end
+  def show2
+    @msg = params[:msg]
+    @msg2 = params[:msg2]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'messages/:msg' => 'messages#show'
+
+  get 'messages/:msg/:msg2' => 'messages#show2'
 end
